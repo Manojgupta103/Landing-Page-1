510269bb82b9f4733506c1ffe2bd79513791bf86
Manya103 <113758890+Manya103@users.noreply.github.com>
2023-09-19 23:39:09 +0530

    Delete Level 1 Task 1 Landin Page directory

--- Level 1 Task 1 Landin Page/index.js
+++
@@ -1,8 +0,0 @@
-function openNavbar() {
-	document.getElementById("sideNavigationBar")
-		.style.width = "50%";
-}
-function closeNavbar() {
-	document.getElementById("sideNavigationBar")
-		.style.width = "0%";
-}
